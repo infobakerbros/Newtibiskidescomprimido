4951da72cfe012ace0ce38f70d1bee4abe4f2934
infobakerbros <info.bakerbros@gmail.com>
2025-11-08 21:13:16 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-4xl font-bold text-tibiskiGold">Tibiski Portal Listo</h1>
+      <p className="mt-4 text-tibiskiBeige">Proyecto listo para subir a GitHub o Vercel 🚀</p>
+    </div>
+  );
+}
